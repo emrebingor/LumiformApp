6b46c50e8c4b6598a4d547d98ed8fdbcc53e46ff
emrebingor <emrebingor5@gmail.com>
2025-07-06 12:56:22 +0300

    Initial commit

---
+++ LumiformApp/LumiformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiformAppApp.swift
+//  LumiformApp
+//
+//  Created by Emre Bingor on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LumiformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
